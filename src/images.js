a175e25ebe0cad352a5ddd5fca1b67aee62b38ae
Bram <Bram.mathijssen@gmail.com>
2023-06-01 11:44:55 +0200

    feat: added image export script

---
+++ src/images.js
@@ -0,0 +1,18 @@
+import image1 from "./assets/images/red-bull-melbourne.jpg";
+import image2 from "./assets/images/champions-league.jpg";
+import image3 from "./assets/images/el-clasico.jpg";
+import image4 from "./assets/images/japan-gp.jpg";
+import image5 from "./assets/images/john-mayer.jpg";
+import image6 from "./assets/images/league-of-legends.jpg";
+import image7 from "./assets/images/valorant.jpg";
+import image8 from "./assets/images/wimbledon.jpg";
+
+export const events = [
+    { name: "hey", image: image1 },
+    { name: "hey2", image: image2 },
+    { name: "hey3", image: image3 },
+    { name: "hey4", image: image4 },
+    { name: "hey5", image: image5 },
+];
+
+export default [image1, image2, image3, image4, image5, image6, image7, image8];
