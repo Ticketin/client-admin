1215a44377f17cab45c588108225ab5583ae0358
Bram <Bram.mathijssen@gmail.com>
2023-06-03 17:53:08 +0200

    feat: added constants to support new contracts

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import contractAddresses from "./contractAddresses.json";
+import pockyCollectionsAbi from "./PockyCollections.abi.json";
+import pockyTicketAbi from "./Ticket.abi.json";
+
+export { contractAddresses, pockyCollectionsAbi, pockyTicketAbi };
