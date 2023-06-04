0e170e903e896d0c72c45e858dd6626d54052f5b
Bram <Bram.mathijssen@gmail.com>
2023-06-04 15:37:10 +0200

    build: adjusted vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["nft.storage"],
+      external: ["nft.storage/dist/bundle.esm.min.js"],
     },
   },
 });
