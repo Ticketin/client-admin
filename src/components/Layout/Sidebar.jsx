2acb514416a537bfc024c3786528922e24efc61c
Bram <Bram.mathijssen@gmail.com>
2023-06-04 14:09:09 +0200

    style: minor change in sidebar nav

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -4,30 +4,39 @@ import styles from "./Sidebar.module.scss";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <aside className={styles.sidebar}>
-            <nav className={styles.mainNavigation}>
-                <ul className={styles.ul}>
-                    <li className={styles.li}>
-                        <NavLink className={(navData) => (navData.isActive ? styles.active : "")} to={"/event-list"}>
-                            NFT List
-                        </NavLink>
-                    </li>
-                    <li className={styles.li}>
-                        <NavLink className={(navData) => (navData.isActive ? styles.active : "")} to={"/activated"}>
-                            Activated NFTs
-                        </NavLink>
-                    </li>
+  return (
+    <aside className={styles.sidebar}>
+      <nav className={styles.mainNavigation}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <NavLink
+              className={(navData) => (navData.isActive ? styles.active : "")}
+              to={"/event-list"}
+            >
+              Event List
+            </NavLink>
+          </li>
+          <li className={styles.li}>
+            <NavLink
+              className={(navData) => (navData.isActive ? styles.active : "")}
+              to={"/activated"}
+            >
+              Activated Events
+            </NavLink>
+          </li>
 
-                    <li className={styles.li}>
-                        <NavLink className={(navData) => (navData.isActive ? styles.active : "")} to={"/generate-event"}>
-                            Generate
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </aside>
-    );
+          <li className={styles.li}>
+            <NavLink
+              className={(navData) => (navData.isActive ? styles.active : "")}
+              to={"/generate-event"}
+            >
+              Generate
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
