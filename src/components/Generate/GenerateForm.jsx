b18e094ea33dcdfcc793dfe56322612ac7c7859c
Bram <Bram.mathijssen@gmail.com>
2023-06-04 13:58:22 +0200

    feat: added functionality to register new events on chain

--- src/components/Generate/GenerateForm.jsx
+++ src/components/Generate/GenerateForm.jsx
@@ -2,99 +2,204 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 import InputField from "../UI/InputField";
 import styles from "./GenerateForm.module.scss";
+import Dropdown from "../UI/Dropdown";
+import DateField from "../UI/DateField";
+import { NFTStorage } from "nft.storage";
+import { useEffect } from "react";
+import { pockyCollectionsAbi } from "../../constants";
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { useState } from "react";
 
-const GenerateForm = () => {
-    const {
-        register,
-        formState: { errors },
-        handleSubmit,
-        watch,
-    } = useForm({
-        mode: "all", // "onChange" + "onBlur"
-    });
+const GenerateForm = ({ uploadedImage }) => {
+  const API_KEY = import.meta.env.VITE_NFTSTORAGE_API_KEY;
+  const client = new NFTStorage({ token: API_KEY });
 
-    // All variables from the form which we want to pass as arguments to the deployNewTicketCollection() function
-    const eventName = watch("eventName", false); // false is defeault value
-    const eventSymbol = watch("eventSymbol", false);
-    const eventDescription = watch("eventDescription", false);
-    const eventDate = watch("eventDate", false);
-    const ticketAmount = watch("ticketAmount", false); // should be passed to constructor
-    const ticketPrice = watch("ticketPrice", false);
-    const ticketImage = watch("ticketImage", false);
+  const [imageUploaded, setImageUploaded] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
 
-    const onSubmit = async (data) => {
-        console.log("adding new event");
-        console.log(data);
-    };
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm({
+    mode: "all", // "onChange" + "onBlur"
+  });
 
-    return (
-        <>
-            <form className={styles.registerTicketForm} onSubmit={handleSubmit(onSubmit)}>
-                <InputField
-                    type="text"
-                    name="eventName"
-                    label="Event Name"
-                    placeholder="Enter your event name here"
-                    errors={errors}
-                    register={register}
-                    validationSchema={{
-                        required: "Event Name is required",
-                    }}
-                    required
-                />
-                <InputField
-                    type="text"
-                    name="eventDescription"
-                    label="Event Description"
-                    placeholder="Enter your event description here"
-                    errors={errors}
-                    register={register}
-                    validationSchema={{
-                        required: "Event Description is required.",
-                    }}
-                    required
-                />
-                <InputField
-                    type="text"
-                    name="eventDate"
-                    label="Event Date"
-                    placeholder="Enter the begin and end date of your event here"
-                    errors={errors}
-                    register={register}
-                    validationSchema={{
-                        required: "Event Date is required.",
-                    }}
-                    required
-                />
-                <InputField
-                    type="number"
-                    name="ticketPrice"
-                    label="Single Ticket Price"
-                    placeholder="Enter the price per ticket of your event here"
-                    errors={errors}
-                    register={register}
-                    validationSchema={{
-                        required: "Ticket price is required",
-                        maxLength: { value: 32, message: "Can't be more than 32 characters" },
-                    }}
-                    required
-                />
-                <InputField
-                    type="number"
-                    name="ticketAmount"
-                    label="Total Ticket Amount"
-                    placeholder="Enter the total amount of tickets here"
-                    errors={errors}
-                    register={register}
-                    validationSchema={{
-                        required: "Ticket amount is required",
-                        maxLength: { value: 32, message: "Can't be more than 32 characters" },
-                    }}
-                    required
-                />
-            </form>
-        </>
-    );
+  console.log(uploadedImage);
+
+  const watchName = watch("name", false); // false is defeault value
+  const watchDescription = watch("description", false);
+  const watchCategory = watch("category", false);
+  const watchStartDate = watch("startDate", false);
+  const watchEndDate = watch("endDate", false);
+  const watchLocation = watch("location", false);
+
+  const {
+    config,
+    data: prepareData,
+    error: prepareError,
+    isError: isPrepareError,
+    refetch: refetchPrepare,
+  } = usePrepareContractWrite({
+    address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    abi: pockyCollectionsAbi,
+    functionName: "register",
+    enabled: imageUploaded,
+    args: [
+      [
+        watchName,
+        watchCategory,
+        "x", // default value for dateText
+        dateTimeToTimestamp(watchStartDate),
+        dateTimeToTimestamp(watchEndDate),
+        watchLocation,
+        watchDescription,
+        imageUrl,
+        "x", // default value for backgroundUrl
+        false, // default value for featured
+        false, // default value for updated
+        "x", // default value for eventResult
+      ],
+    ],
+  });
+
+  const { data, error, isError, write } = useContractWrite(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+    onSuccess(data) {
+      console.log(`finished`);
+      console.log(data);
+    },
+  });
+
+  const uploadImageToIPFS = async (uploadedImage) => {
+    const imageCid = await client.storeBlob(uploadedImage[0]);
+    const ipfsImageUrl = `https://ipfs.io/ipfs/${imageCid}`;
+    setImageUrl(ipfsImageUrl);
+  };
+
+  function dateTimeToTimestamp(dateTime) {
+    const date = new Date(dateTime);
+    const timestamp = Math.floor(date.getTime() / 1000);
+    return timestamp;
+  }
+
+  // uploads image to IPFS, and sets ImageUpload to true to trigger effect
+  const onSubmit = async (data) => {
+    await uploadImageToIPFS(uploadedImage);
+    setImageUploaded(true);
+    console.log(`image uploaded true`);
+  };
+
+  // wagmi stuff - refetch the prepare date (to get most recent imageIPFS CID from state)
+  useEffect(() => {
+    if (imageUploaded == false) return;
+    console.log(`refetched prepare...`);
+    refetchPrepare();
+    console.log(`done refetching`);
+  }, [imageUploaded]);
+
+  // wagmi stuff - writes register function after prepareData has changed from previous effect
+  useEffect(() => {
+    if (imageUploaded == false) return;
+    console.log(`trying to write..`);
+    write?.();
+    console.log(`written..`);
+  }, [prepareData]);
+
+  return (
+    <>
+      <form
+        id="generate-form"
+        className={styles.registerTicketForm}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <InputField
+          type="text"
+          name="name"
+          label="Event Name"
+          placeholder="Enter your event name here"
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "Event Name is required",
+          }}
+          required
+        />
+        <InputField
+          type="text"
+          name="description"
+          label="Event Description"
+          placeholder="Enter your event description here"
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "Event Description is required.",
+          }}
+          required
+        />
+        <Dropdown
+          name="category"
+          label="Category"
+          register={register}
+          options={[
+            { type: "NBA", value: "NBA" },
+            { type: "UFC", value: "UFC" },
+          ]}
+          errors={errors}
+          validationSchema={{
+            required: "Role is required.",
+          }}
+          required
+        />
+        <DateField
+          type="text"
+          name="startDate"
+          label="Start Date"
+          placeholder="Enter the starting date and time here"
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "Event Start Date is required.",
+          }}
+          required
+        />
+        <DateField
+          type="text"
+          name="endDate"
+          label="End Date"
+          placeholder="Enter the end date and time here"
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "Event End Date is required.",
+          }}
+          required
+        />
+        <InputField
+          type="text"
+          name="location"
+          label="Event Location"
+          placeholder="Enter the location of your event here"
+          errors={errors}
+          register={register}
+          validationSchema={{
+            required: "Location is required",
+          }}
+          required
+        />
+      </form>
+    </>
+  );
 };
 
 export default GenerateForm;
--- src/components/Generate/UploadImage.jsx
+++ src/components/Generate/UploadImage.jsx
@@ -5,131 +5,144 @@ import plusIcon from "./../../assets/plusIcon.svg";
 import styles from "./UploadImage.module.scss";
 
 const baseStyle = {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    borderWidth: 2,
-    borderRadius: 8,
-    borderColor: "rgb(203 213 225)",
-    borderStyle: "dashed",
-    backgroundColor: "rgb(241 245 249)",
-    color: "#bdbdbd",
-    outline: "none",
-    transition: "border .24s ease-in-out",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  borderWidth: 2,
+  borderRadius: 8,
+  borderColor: "rgb(203 213 225)",
+  borderStyle: "dashed",
+  backgroundColor: "rgb(241 245 249)",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
 };
 
 const focusedStyle = {
-    borderColor: "#2196f3",
+  borderColor: "#2196f3",
 };
 
 const acceptStyle = {
-    borderColor: "#00e676",
+  borderColor: "#00e676",
 };
 
 const rejectStyle = {
-    borderColor: "#ff1744",
+  borderColor: "#ff1744",
 };
 
 // image preview
 const thumbsContainer = {
-    display: "flex",
-    height: "100%",
-    width: "100%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    marginTop: 16,
+  display: "flex",
+  height: "100%",
+  width: "100%",
+  flexDirection: "row",
+  flexWrap: "wrap",
 };
 
 const thumb = {
-    display: "inline-flex",
-    borderRadius: 2,
-    border: "1px solid #eaeaea",
-    width: "100%",
-    height: "100%",
-    boxSizing: "border-box",
+  display: "inline-flex",
+  borderRadius: 2,
+  objectFit: "contain",
+  border: "1px solid #eaeaea",
+  width: "100%",
+  height: "100%",
+  boxSizing: "border-box",
 };
 
 const thumbInner = {
-    display: "flex",
-    minWidth: 0,
-    overflow: "hidden",
+  objectFit: "contain",
+  display: "flex",
+  overflow: "hidden",
 };
 
 const img = {
-    display: "block",
-    borderRadius: "8px",
-    width: "auto",
-    height: "100%",
+  objectFit: "cover",
+  borderRadius: "8px",
+  width: "100%",
+  height: "100%",
 };
 
-const UploadImage = () => {
-    const [files, setFiles] = useState([]);
-    const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject, acceptedFiles } = useDropzone({
-        maxFiles: 1,
-        accept: { "image/*": [] },
-        onDrop: (acceptedFiles) => {
-            setFiles(
-                acceptedFiles.map((file) =>
-                    Object.assign(file, {
-                        preview: URL.createObjectURL(file),
-                    })
-                )
-            );
-        },
-    });
+const UploadImage = ({ onUpload }) => {
+  const [files, setFiles] = useState([]);
+  const {
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+    acceptedFiles,
+  } = useDropzone({
+    maxFiles: 1,
+    accept: { "image/*": [] },
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
 
-    const thumbs = files.map((file) => (
-        <div style={thumb} key={file.name}>
-            <div style={thumbInner}>
-                <img
-                    src={file.preview}
-                    style={img}
-                    // Revoke data uri after image is loaded
-                    onLoad={() => {
-                        URL.revokeObjectURL(file.preview);
-                    }}
-                />
-            </div>
+  useEffect(() => {
+    if (!files) return;
+    console.log(`setting files`);
+    onUpload(files);
+  }, [files]);
+
+  const thumbs = files.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img
+          src={file.preview}
+          style={img}
+          // Revoke data uri after image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
+      </div>
+    </div>
+  ));
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, []);
+
+  return (
+    <section className={styles.container}>
+      <div {...getRootProps({ style })}>
+        <div className={styles.content}>
+          <input {...getInputProps()} />
+          {console.log(acceptedFiles)}
+          {acceptedFiles.length > 0 ? (
+            <div style={thumbsContainer}>{thumbs}</div>
+          ) : (
+            <>
+              <img src={plusIcon} />
+              <p className={styles.uploadImageText}>Upload your Image</p>
+              <p className={styles.fileTypeText}>
+                PNG and JPG files are allowed
+              </p>
+            </>
+          )}
         </div>
-    ));
-
-    const style = useMemo(
-        () => ({
-            ...baseStyle,
-            ...(isFocused ? focusedStyle : {}),
-            ...(isDragAccept ? acceptStyle : {}),
-            ...(isDragReject ? rejectStyle : {}),
-        }),
-        [isFocused, isDragAccept, isDragReject]
-    );
-
-
-    useEffect(() => {
-        // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
-        return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
-    }, []);
-
-    return (
-        <section className={styles.container}>
-            <div {...getRootProps({ style })}>
-                <div className={styles.content}>
-                    <input {...getInputProps()} />
-                    {console.log(acceptedFiles)}
-                    {acceptedFiles.length > 0 ? (
-                        <div style={thumbsContainer}>{thumbs}</div>
-                    ) : (
-                        <>
-                            <img src={plusIcon} />
-                            <p className={styles.uploadImageText}>Upload your Image</p>
-                            <p className={styles.fileTypeText}>PNG and JPG files are allowed</p>
-                        </>
-                    )}
-                </div>
-            </div>
-
-        </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default UploadImage;
