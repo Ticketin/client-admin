c679bb3f15a3581bc133a758a45b117e6d8605df
Bram <Bram.mathijssen@gmail.com>
2023-06-08 04:51:16 +0200

    feat: added new modal which can be used by admin to withdraw funds per event

---
+++ src/components/EventList/ModalDetails.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Modal from "react-modal";
+
+import styles from "./ModalDetails.module.scss";
+import ModalForm from "./ModalForm";
+import ModalUploadImage from "./ModalUploadImage";
+import Button from "../UI/Button";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import ModalContent from "./ModalContent";
+
+const ModalDetails = ({ show, closeModal, eventId }) => {
+  return (
+    <div>
+      <Modal
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+        isOpen={show}
+        onRequestClose={closeModal}
+        shouldCloseOnOverlayClick={true}
+        shouldReturnFocusAfterClose={true}
+        shouldCloseOnEsc={true}
+        contentLabel="Example Modal"
+      >
+        <div className={styles.titleRow}>
+          <h4>Event Details</h4>
+          <button
+            className={styles.iconButton}
+            aria-label="Close"
+            onClick={closeModal}
+          >
+            <Cross2Icon />
+          </button>
+        </div>
+        <div className={styles.modalContent}>
+          <div className={styles.formColumn}>
+            <ModalContent eventId={eventId} />
+          </div>
+          <div className={styles.imageColumn}>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalDetails;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
 import contractAddresses from "./contractAddresses.json";
 import pockyCollectionsAbi from "./PockyCollections.abi.json";
 import pockyTicketAbi from "./Ticket.abi.json";
-import pockyTicketSales from "./PockyTicketSales.abi.json";
+import pockyTicketSalesAbi from "./PockyTicketSales.abi.json";
 
 export {
   contractAddresses,
   pockyCollectionsAbi,
   pockyTicketAbi,
-  pockyTicketSales,
+  pockyTicketSalesAbi,
 };
